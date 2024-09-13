1bbcc5e676cf1db25fa5b4e8a8d9ddfa1ab9b472
haseebrepo <chhaseebmehar52@gmail.com>
2024-09-13 14:09:55 +0200

    kubernetes deployment cluster

--- ecom/settings.py
+++ ecom/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 SITE_URL = 'http://127.0.0.1:8000'
 
-ALLOWED_HOSTS = []
+# import socket
+# ALLOWED_HOSTS = [socket.gethostbyname('localhost')]
+# print("here are the allowed hosts")
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
